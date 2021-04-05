232659edf66351d893920258966e2728e825acaa
JaeYoonMyung <audwodbs492@gmail.com>
2021-04-06 07:18:08 +0900

    Update PostsRepositoryTest.java

--- src/test/java/com/coffe/web/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/coffe/web/springboot/domain/posts/PostsRepositoryTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//todo 96page
+//todo 96p
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostsRepositoryTest {
