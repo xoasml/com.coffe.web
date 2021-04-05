1ea6f2936f0d94613bb0f5d1026362bf60658ef8
JaeYoonMyung <audwodbs492@gmail.com>
2021-04-06 07:28:00 +0900

    Update PostsRepositoryTest.java
    주석수정

--- src/test/java/com/coffe/web/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/coffe/web/springboot/domain/posts/PostsRepositoryTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//todo 96p
+//todo 96page
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostsRepositoryTest {
