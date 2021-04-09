d798ad6667b6359c728cd724cc34b7bc5461c887
JaeYoonMyung <audwodbs492@gmail.com>
2021-04-09 19:54:12 +0900

    공지사항 컨트롤러 교통정리.

--- src/main/java/com/coffe/web/springboot/web/noticeController.java
+++ src/main/java/com/coffe/web/springboot/web/noticeController.java
@@ -1,4 +1,15 @@
 package com.coffe.web.springboot.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class noticeController {
+    /*
+     *  todo 상세보기 기능은 여기에 만들고
+     * */
+    @GetMapping("/noticeDetails")
+    public String noticeDetails(){
+        return "notice";
+    }
 }
