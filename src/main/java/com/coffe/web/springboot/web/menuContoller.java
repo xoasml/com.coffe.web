75ea9fcf45d627d06c2d988c42642b79f156a6f3
JaeYoonMyung <audwodbs492@gmail.com>
2021-04-13 06:56:23 +0900

    Revert "Merge branch 'main' into notice"
    This reverts commit e45ac440256205a20d582708585c0380ef062e05, reversing
    changes made to ec7072164c1c9e7f84f3bdf518384bb80767aded.

--- src/main/java/com/coffe/web/springboot/web/menuContoller.java
+++ src/main/java/com/coffe/web/springboot/web/menuContoller.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 @Controller
 public class menuContoller {
-/*
-*  todo 여기는 화면로딩 만들기
-* */
+
     @GetMapping("/notice")
     public String notice(){
         return "notice";
