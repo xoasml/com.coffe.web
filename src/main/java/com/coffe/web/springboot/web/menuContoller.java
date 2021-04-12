65c99f1182487b4719bd701daca460092869209f
JaeYoonMyung <audwodbs492@gmail.com>
2021-04-13 07:53:59 +0900

    4/13 NOTICE 재 창조

--- src/main/java/com/coffe/web/springboot/web/menuContoller.java
+++ src/main/java/com/coffe/web/springboot/web/menuContoller.java
@@ -1,15 +1,33 @@
 package com.coffe.web.springboot.web;
 
+import com.coffe.web.springboot.config.auth.dto.SessionUser;
+import com.coffe.web.springboot.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class menuContoller {
 
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    /*
+     *  todo 여기는 화면로딩 만들기
+     * */
+
     @GetMapping("/notice")
-    public String notice(){
+    public String notice(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "notice";
     }
 }
